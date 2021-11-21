acb74c752fa2abc4ecf0138819bb591b66825189
brainTrain <abductee@gmail.com>
2021-11-20 21:55:29 -0800

    use srcdoc to load 3m83d bundled html into iframe

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import '3m83d';
+const page = require('3m83d');
 import _ from 'lodash';
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('main');
+  element.style = 'display: flex; justify-content: center; align-items: center;';
 
-  element.innerHTML = 'javascript loaded';
+  const iframe = document.createElement('iframe');
+  iframe.style = 'width: 90vw; height: 90vh; box-sizing: border-box;';
+  iframe.srcdoc = page.default;
+
+  element.appendChild(iframe);
 
   return element;
 }
